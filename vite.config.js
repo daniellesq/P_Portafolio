354ee9d41e0e732b02aff0f716527a6fd57b29f7
Daniel Esquivel <danielesquivel@MacBook-Air-de-Daniel.local>
2025-11-18 05:05:45 -0600

    Configurar portafolio y dependencias

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Mi_Portafolio/", 
+  base: "/P_Portafolio/", 
   plugins: [react(), tailwindcss()],
 });
 
