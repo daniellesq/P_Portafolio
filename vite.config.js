a08d2c14ff2b569d43daad1e6e41259256c0e968
Daniel Esquivel <danielesquivel@MacBook-Air-de-Daniel.local>
2025-11-18 04:45:10 -0600

    Archivos Portafolio

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-// vite.config.js o vite.config.mts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), 
-  ],
+  base: "/Mi_Portafolio/", 
+  plugins: [react(), tailwindcss()],
 });
+
